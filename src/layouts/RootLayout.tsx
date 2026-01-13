6257cc286e04cc81dfa9be9ad55c803b641ee39e
kkimsxu <jisue0567@hanyang.ac.kr>
2026-01-13 20:00:08 +0900

    feat: 모바일 루트 레이아웃 설정

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,18 @@
+import type { PropsWithChildren } from "react";
+
+export function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="min-h-svh bg-gray-50">
+      <div
+        className="
+          mx-auto min-h-svh w-full max-w-[480px]
+          bg-white
+          px-5
+          pt-[env(safe-area-inset-top)]
+          pb-[env(safe-area-inset-bottom)]
+        ">
+        {children}
+      </div>
+    </div>
+  );
+}
