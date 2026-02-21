7832cd1f2e8a5f009977d08d9b9fe7e25ef4371e
kkimsxu <jisue0567@hanyang.ac.kr>
2026-02-21 19:50:56 +0900

    Feat: 계좌 연동 페이지 생성

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-2xl flex-col items-center justify-center px-6 text-center">
+      <h1 className="text-3xl font-bold">주식 계좌 연동</h1>
+      <p className="mt-3 text-muted-foreground">주식 계좌 연동 페이지입니다.</p>
+    </div>
+  );
+}
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home/Home";
+import Login from "@/pages/Login/Login";
 import StockDetail from "@/pages/StockDetail/StockDetail";
 import TradeDetail from "@/pages/TradeDetail/TradeDetail";
 
 export default function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 1. 홈 */}
-        <Route path="/" element={<Home />} />
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* 1. 홈 */}
+				<Route path="/" element={<Home />} />
 
-        {/* 2. 종목 상세 */}
-        <Route path="/stocks/:stockId" element={<StockDetail />} />
+				{/* 2. 종목 상세 */}
+				<Route path="/stocks/:stockId" element={<StockDetail />} />
 
-        {/* 3. 거래내역 상세 */}
-        <Route path="/trades/:userId" element={<TradeDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+				{/* 3. 거래내역 상세 */}
+				<Route path="/trades/:userId" element={<TradeDetail />} />
+
+				{/* 4. 주식 계좌 연동 로그인 */}
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
